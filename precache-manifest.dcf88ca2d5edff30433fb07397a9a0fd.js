a2115d7a923e4b3910e48f41d839e4a241a84fd7
shridharwebs <spwebs3@gmail.com>
2020-08-10 16:57:38 +0530

    Updates

---
+++ precache-manifest.dcf88ca2d5edff30433fb07397a9a0fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3160b1c52dffba081634ce4056cd515",
+    "url": "/infolio/index.html"
+  },
+  {
+    "revision": "893e25477f62a5ff1467",
+    "url": "/infolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "180fb55545c6fe0b44c9",
+    "url": "/infolio/static/js/2.d74849c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infolio/static/js/2.d74849c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893e25477f62a5ff1467",
+    "url": "/infolio/static/js/main.1891b147.chunk.js"
+  },
+  {
+    "revision": "8c931c911d5c0cddc031",
+    "url": "/infolio/static/js/runtime-main.d81c3109.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/infolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
