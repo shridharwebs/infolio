18701332ff185229750bf51326cd66268b5e7a63
shridharwebs <spwebs3@gmail.com>
2020-08-13 17:33:16 +0530

    Updates

---
+++ precache-manifest.2fd82d07ef5e23d8519050e660d96563.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e72e6d6b5acc940ed7e4a4a898e14b1",
+    "url": "/infolio/index.html"
+  },
+  {
+    "revision": "9b95ed1744b7d6cef1ee",
+    "url": "/infolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "97b4ee56a81281ca2953",
+    "url": "/infolio/static/css/main.ecaac416.chunk.css"
+  },
+  {
+    "revision": "9b95ed1744b7d6cef1ee",
+    "url": "/infolio/static/js/2.7dc46b23.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/infolio/static/js/2.7dc46b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b4ee56a81281ca2953",
+    "url": "/infolio/static/js/main.6781cf1a.chunk.js"
+  },
+  {
+    "revision": "8c931c911d5c0cddc031",
+    "url": "/infolio/static/js/runtime-main.d81c3109.js"
+  },
+  {
+    "revision": "fac08a8aa9a9651879412116e3d215fe",
+    "url": "/infolio/static/media/23.fac08a8a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/infolio/precache-manifest.86c6f06c7109f93cb7a9bb72d2e26c84.js"
+  "/infolio/precache-manifest.2fd82d07ef5e23d8519050e660d96563.js"
 );
 
 self.addEventListener('message', (event) => {
